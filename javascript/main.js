579f906d9ba3344e6f3eecea3ab6472069472c42
z0hnan <simesim2006@gmail.com>
2023-12-07 21:22:37 +0100

    v0.5 - extra styling

--- javascript/main.js
+++ javascript/main.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function addPadding() {
       const navbarWidth = document.querySelector('.navbar').offsetWidth;
       const navbarItemCount = navbarItems.length;
-      const totalPadding = (navbarWidth / navbarItemCount) / 3;
+      const totalPadding = (navbarWidth / navbarItemCount) / 4;
   
       navbarItems.forEach(item => {
         item.style.paddingLeft = totalPadding + 'px';
@@ -15,5 +15,18 @@ document.addEventListener('DOMContentLoaded', function() {
   
     window.addEventListener('resize', addPadding);
     addPadding();
+
+    window.onscroll = function() {
+        if (window.scrollY > 0) {
+       console.log(window.scrollY);
+        document.body.style.background = "linear-gradient(rgba(0, 0, 0, " + window.scrollY/1080 + "), rgba(0, 0, 0, " + window.scrollY/1080 + ")), url(/images/FSLogo1440p.png) no-repeat center center fixed";
+        document.body.style.backgroundSize = "cover";
+        }
+        else {
+        document.body.style.background = "linear-gradient(rgba(0, 0, 0, " + 0 + "), rgba(0, 0, 0, " + 0 + ")), url(/images/FSLogo1440p.png) no-repeat center center fixed";
+        document.body.style.backgroundSize = "cover";
+
+        }
+      }
 
   });
